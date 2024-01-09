100d2fd68e602468a5e91f5a902c75027170423a
Samuel Fontes <samuguel@hotmail.com.br>
2024-01-09 20:01:46 -0300

    Making map fields private

--- Assets/Scripts/GameEntities/Map.cs
+++ Assets/Scripts/GameEntities/Map.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    public string Description;
-    public Gamemode Gamemode;
-    public Vector2 TopRight; // Used to stop the camera and stuff going outside the map
-    public Vector2 BottomLeft;
+    [field:SerializeField] public string Description { get; private set; }
+    [field: SerializeField] public Gamemode Gamemode { get; private set; }
+    [field: SerializeField] public Vector2 TopRight { get; private set; }// Used to stop the camera and stuff going outside the map
+    [field: SerializeField] public Vector2 BottomLeft {  get; private set; }
 }
