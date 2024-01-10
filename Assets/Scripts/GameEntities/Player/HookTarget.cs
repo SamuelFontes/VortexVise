5a7bcc7fea2dd318d9b6f640b40551a0d9d83a38
Samuel Fontes <samuguel@hotmail.com.br>
2024-01-10 09:51:12 -0300

    Refactored HookTarget

--- Assets/Scripts/GameEntities/Player/HookTarget.cs
+++ Assets/Scripts/GameEntities/Player/HookTarget.cs
@@ -3,55 +3,32 @@ using UnityEngine.InputSystem;
 
 public class HookTarget : MonoBehaviour
 {
-    public GameObject player;
-    public float crossHairDistance = 4.5f;
+    [SerializeField] private Player _player;
+    [SerializeField] private float _crossHairDistance = 4.5f;
+    private bool _useMouse = false;
 
-    private bool useMouse = false;
-    public bool aiming = false;
-
-
-    private void Start()
+    private void OnAim(InputValue inputValue)
     {
+        var targetDirection = inputValue.Get<Vector2>();
+
+        // This is for using the hook, only aim in 8ish directions
+        targetDirection.y = QuantizeAxis(targetDirection.y);
+        targetDirection.x = QuantizeAxis(targetDirection.x);
+
+        // When moving foward it will hook upwards
+        if (targetDirection.y == 0 && (targetDirection.x == 1 || targetDirection.x == -1))
+            targetDirection.y++;
+
+        Vector2 offset = (Vector2)_player.transform.position + (targetDirection * _crossHairDistance);
+
+        transform.position = new Vector2(offset.x, offset.y);
     }
-    private void Update()
-    {
-        if(useMouse)
-            transform.position = Utils.GetMousePosition();
 
-    }
-    void OnAim(InputValue inputValue)
-    {
-        if (!aiming)
-        {
-            var targetDirection = inputValue.Get<Vector2>();
-
-            // This is for using the hook, only aim in 8ish directions
-            targetDirection.y = QuantizeAxis(targetDirection.y);
-            targetDirection.x = QuantizeAxis(targetDirection.x);
-
-
-            // When moving foward it will hook upwards
-            if (targetDirection.y == 0 && (targetDirection.x == 1 || targetDirection.x == -1))
-                targetDirection.y++;
-
-            Vector2 offset = (Vector2)player.transform.position + (targetDirection * crossHairDistance);
-
-
-            transform.position = new Vector2(offset.x, offset.y);
-        }
-    }
-    int QuantizeAxis ( float input)
+    private int QuantizeAxis ( float input)
     {
         if (input < -0.5f) return -1;
         if (input > 0.3f) return 1;
         return 0;
     }
 
-    void OnMousePosition(InputValue inputValue)
-    {
-        GetComponent<SpriteRenderer>().enabled = true;
-        Vector2 targetPosition = Utils.GetMousePosition();
-        transform.position = targetPosition;
-        useMouse = true;
-    }
 }
