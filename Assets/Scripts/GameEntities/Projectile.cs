051d4841aae7ad6e33882f84ac9e02a138666e7a
SamuelFontes <samuguel@hotmail.com.br>
2024-01-10 00:32:58 -0600

    Destroying bullets when outside the map

--- Assets/Scripts/GameEntities/Projectile.cs
+++ Assets/Scripts/GameEntities/Projectile.cs
@@ -8,6 +8,7 @@ public class Projectile : MonoBehaviour
     private Vector2 _direction;
     private float _force;
     private Rigidbody2D _rigidbody;
+    private Transform _transform;
 
     public void Init(float baseDamage, GameObject explosion, Team team, Vector2 direction, float force)
     {
@@ -24,7 +25,9 @@ public class Projectile : MonoBehaviour
 
     void Update()
     {
-        
+        _transform = transform;
+        DestroyWhenOutsideTheMap();
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -40,5 +43,16 @@ public class Projectile : MonoBehaviour
     void ApplyTeam()
     {
         gameObject.layer = _team.GetProjectileTeamLayer();
+    }
+
+    void DestroyWhenOutsideTheMap()
+    {
+        if (_transform.position.x > GameState.Instance.CurrentMap.TopRight.x
+        || _transform.position.y > GameState.Instance.CurrentMap.TopRight.y
+        || _transform.position.x < GameState.Instance.CurrentMap.BottomLeft.x
+        || _transform.position.y < GameState.Instance.CurrentMap.BottomLeft.y)
+        {
+            GameObject.Destroy(gameObject);
+        }
     }
 }
