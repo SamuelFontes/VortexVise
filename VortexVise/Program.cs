59cfccc1c852be8c2aa813ea651c16e95fe1983b
Samuel Fontes <samuguel@hotmail.com.br>
2024-01-19 18:15:44 -0300

    Character moving on the screen

--- VortexVise/Program.cs
+++ VortexVise/Program.cs
@@ -2,9 +2,10 @@
 using System.Numerics;
 using System.Reflection;
 
-int screenWidth = 800;
-int screenHeight = 480;
+int screenWidth = 1920;
+int screenHeight = 1080;
 Raylib.InitWindow(screenWidth, screenHeight, "Vortex Vise");
+Raylib.ToggleFullscreen();
 
 
 Texture2D player = Raylib.LoadTexture("Resources/Sprites/Skins/fatso.png");
@@ -12,32 +13,48 @@ Texture2D player = Raylib.LoadTexture("Resources/Sprites/Skins/fatso.png");
 Vector2 playerPosition = new Vector2( screenWidth/2, screenHeight/2 );
 //Raylib.SetTargetFPS(60);               // TODO: Implement deltatime 
 
+// Source rectangle (part of the texture to use for drawing)
 
-float playerVelocity = 0f;
+// Destination rectangle (screen rectangle where drawing part of texture)
+
+float playerGravitacionalForce = 0f;
+int playerDirection = 1;
+float playerMoveSpeed = 450;
 while (!Raylib.WindowShouldClose())
 {
     float deltaTime = Raylib.GetFrameTime();
 
     Raylib.DrawText("FPS: " + (int)(1f/deltaTime), 12, 12, 20, Color.BLACK);
 
-    playerVelocity += 20f * deltaTime;
-    playerPosition.Y += playerVelocity;
+    playerGravitacionalForce += 20f * deltaTime;
+    playerPosition.Y -= playerGravitacionalForce;
     var playerFeet = player.Height + playerPosition.Y;
-    if(playerFeet > screenHeight)
+
+    if(playerFeet > (screenHeight / 2 * -1)  )
     {
-        playerVelocity = 0f;
-        playerPosition.Y = screenHeight - player.Height;
+        playerGravitacionalForce = 0f;
+        playerPosition.Y = (screenHeight / 2 * -1)+ player.Height;
     }
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerPosition.X += 500.0f * deltaTime;
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+    {
+        playerPosition.X -= playerMoveSpeed * deltaTime;
+        playerDirection = -1;
+    }
     if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
     {
-        playerPosition.X -= 500.0f * deltaTime;
+        playerPosition.X += playerMoveSpeed * deltaTime;
+        playerDirection = 1;
     }
 
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.WHITE);
 
-    Raylib.DrawTextureV(player, playerPosition, Color.WHITE);
+    Rectangle sourceRec = new Rectangle( 0.0f, 0.0f, player.Width * playerDirection, player.Height);
+
+    Rectangle destRec = new Rectangle ( screenWidth/2.0f, screenHeight/2.0f, player.Width, player.Height);
+
+    Raylib.DrawTexturePro(player,sourceRec,destRec, playerPosition,0, Color.WHITE);
 
     Raylib.EndDrawing();
 }
