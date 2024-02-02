40f71bfc2b58d279f4b156d9e66c1fde5230e059
Samuel Fontes <samuguel@hotmail.com.br>
2024-02-02 10:57:23 -0300

    Trying to make server work

--- VortexViseServer/Program.cs
+++ VortexViseServer/Program.cs
@@ -1,4 +1,46 @@
-﻿using VortexViseServer;
+﻿using Raylib_cs;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using VortexVise.States;
 
-GameServer gameServer = new GameServer();
-gameServer.Run();
+
+Console.WriteLine("VortexVise Server Started!");
+IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+UdpClient newsock = new UdpClient(ipep);
+
+Console.WriteLine("Waiting for a client...");
+
+IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+int tickrate = 64;
+double deltaTime = 1d / tickrate;
+double currentTime = Raylib.GetTime();
+var lastTime = currentTime;
+
+
+while (true)
+{
+    currentTime = Raylib.GetTime();
+    var time = currentTime - lastTime;
+    
+    // receive everyones input
+    byte[] data = newsock.Receive(ref sender);
+
+    Console.WriteLine("Message received from {0}:", sender.ToString());
+    string receivedData = Encoding.ASCII.GetString(data, 0, data.Length);
+    Console.WriteLine(receivedData);
+
+    if(time > deltaTime)
+    {
+        lastTime = currentTime;
+    }
+    // simulate state
+    var state = GameState.DeserializeState(receivedData);
+
+    var response = state.SerializeState();
+    Console.WriteLine(response);
+
+    byte[] responseData = Encoding.ASCII.GetBytes(response);
+    newsock.Send(responseData, responseData.Length, sender);
+}
