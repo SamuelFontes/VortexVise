634d6e3f298e97f50e2a88d9732e035b63ce825e
Leonardo <lbruni10@gmail.com>
2024-02-04 12:48:44 -0300

    Changed player state to new List (C# Preview version conflict)

--- VortexVise/Logic/GameClient.cs
+++ VortexVise/Logic/GameClient.cs
@@ -9,7 +9,7 @@ namespace VortexVise.Logic
 {
     public class GameClient
     {
-        string ip = "26.121.116.251";
+        string ip = "26.54.110.241";
 /*        string ip = "localhost";
 */        int port = 9050;
         public bool IsConnected = false;
--- VortexVise/States/GameState.cs
+++ VortexVise/States/GameState.cs
@@ -8,7 +8,7 @@ public class GameState
 {
     public double CurrentTime { get; set; }
     public float Gravity { get; set; }
-    public List<PlayerState> PlayerStates { get; set; } = [];
+    public List<PlayerState> PlayerStates { get; set; } = new List<PlayerState>();
 
     // TODO: Add compression
     public string SerializeState()
