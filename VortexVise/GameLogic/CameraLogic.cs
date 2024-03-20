92634b0aa5ab0eb2c88c0209f773cb1c4df2c34e
Samuel Fontes <samuguel@hotmail.com.br>
2024-03-20 12:25:04 -0500

    Split screen working

--- VortexVise/GameLogic/CameraLogic.cs
+++ VortexVise/GameLogic/CameraLogic.cs
@@ -17,29 +17,100 @@ public static class CameraLogic
     public static List<PlayerCamera> PlayerCameras { get; set; } = new List<PlayerCamera>();
     public static void Init()
     {
+        // Setup cameras for split screen, this is kind of a mess but since it's self contained it shouldn't be a problem
         var players = Utils.GetNumberOfLocalPlayers();
         if (players == 1)
         {
-            var cameraOne = new PlayerCamera();
-            cameraOne.RenderTexture = Raylib.LoadRenderTexture(GameCore.GameScreenWidth, GameCore.GameScreenHeight);
-            cameraOne.RenderRectangle = new(0, 0, cameraOne.RenderTexture.texture.width, -cameraOne.RenderTexture.texture.height);
-            var cameraView = new Vector2(GameCore.GameScreenWidth * 0.5f, GameCore.GameScreenHeight * 0.5f);
-            cameraOne.Camera = new Camera2D();
-            cameraOne.Camera.offset = cameraView;
-            cameraOne.Camera.target = cameraView;
-            cameraOne.Camera.zoom = 1;
-            PlayerCameras.Add(cameraOne);
+            var camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture(GameCore.GameScreenWidth, GameCore.GameScreenHeight);
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.5f, 0.5f);
+            PlayerCameras.Add(camera);
         }
         else if (players == 2)
         {
+            // Player 1
+            var camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture((int)(GameCore.GameScreenWidth * 0.5f), GameCore.GameScreenHeight);
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.25f, 0.5f);
+            PlayerCameras.Add(camera);
 
+            // Player 2
+            camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture((int)(GameCore.GameScreenWidth * 0.5f), GameCore.GameScreenHeight);
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.CameraPosition = new((int)(GameCore.GameScreenWidth * 0.5f), 0);
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.25f, 0.5f);
+            PlayerCameras.Add(camera);
         }
         else if (players == 3)
         {
+            // Player 1
+            var camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture((int)(GameCore.GameScreenWidth), (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.5f, 0.25f);
+            PlayerCameras.Add(camera);
 
+            // Player 2
+            camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture((int)(GameCore.GameScreenWidth * 0.5f), (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.CameraPosition = new(0, (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.25f, 0.25f);
+            PlayerCameras.Add(camera);
+
+            // Player 3
+            camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture((int)(GameCore.GameScreenWidth * 0.5f), (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.CameraPosition = new((int)(GameCore.GameScreenWidth * 0.5f), (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.25f, 025f);
+            PlayerCameras.Add(camera);
         }
         else if (players == 4)
         {
+            // Player 1
+            var camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture((int)(GameCore.GameScreenWidth * 0.5f), (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.25f, 0.25f);
+            PlayerCameras.Add(camera);
+
+            // Player 2
+            camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture((int)(GameCore.GameScreenWidth * 0.5f), (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.CameraPosition = new((int)(GameCore.GameScreenWidth * 0.5f), 0);
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.25f, 0.25f);
+            PlayerCameras.Add(camera);
+
+            // Player 3
+            camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture((int)(GameCore.GameScreenWidth * 0.5f), (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.CameraPosition = new(0, (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.25f, 0.25f);
+            PlayerCameras.Add(camera);
+
+            // Player 4
+            camera = new PlayerCamera();
+            camera.RenderTexture = Raylib.LoadRenderTexture((int)(GameCore.GameScreenWidth * 0.5f), (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.RenderRectangle = new(0, 0, camera.RenderTexture.texture.width, -camera.RenderTexture.texture.height);
+            camera.CameraPosition = new((int)(GameCore.GameScreenWidth * 0.5f), (int)(GameCore.GameScreenHeight * 0.5f));
+            camera.Camera = new Camera2D();
+            camera.SetupCamera(0.25f, 0.25f);
+            PlayerCameras.Add(camera);
 
         }
     }
@@ -47,7 +118,7 @@ public static class CameraLogic
     {
         Raylib.EndTextureMode();
         Raylib.BeginTextureMode(PlayerCameras[index].RenderTexture);
-        ProcessCamera(ref targetPosition, ref PlayerCameras[index].Camera);
+        ProcessCamera(ref targetPosition, PlayerCameras[index], ref PlayerCameras[index].Camera);
         Raylib.BeginMode2D(PlayerCameras[index].Camera);
     }
     public static void EndDrawingToCamera(int index)
@@ -65,25 +136,25 @@ public static class CameraLogic
         }
         PlayerCameras.Clear();
     }
-    static void ProcessCamera(ref Vector2 targetPosition, ref Camera2D camera)
+    static void ProcessCamera(ref Vector2 targetPosition, PlayerCamera playerCamera, ref Camera2D camera)
     {
         Vector2 target = new(targetPosition.X, targetPosition.Y);
 
         // Make it stay inside the map
-        if (target.X - GameCore.GameScreenWidth * 0.5f <= 0)
-            target.X = GameCore.GameScreenWidth * 0.5f;
-        else if (target.X + GameCore.GameScreenWidth * 0.5f >= MapLogic.GetMapSize().X)
-            target.X = MapLogic.GetMapSize().X - GameCore.GameScreenWidth * 0.5f;
+        if (target.X - GameCore.GameScreenWidth * playerCamera.CameraOffset.X <= 0)
+            target.X = GameCore.GameScreenWidth * playerCamera.CameraOffset.X;
+        else if (target.X + GameCore.GameScreenWidth * playerCamera.CameraOffset.X >= MapLogic.GetMapSize().X)
+            target.X = MapLogic.GetMapSize().X - GameCore.GameScreenWidth * playerCamera.CameraOffset.X;
 
-        if (target.Y - GameCore.GameScreenHeight * 0.5f <= 0)
-            target.Y = GameCore.GameScreenHeight * 0.5f;
-        else if (target.Y + GameCore.GameScreenHeight * 0.5f >= MapLogic.GetMapSize().Y)
-            target.Y = MapLogic.GetMapSize().Y - GameCore.GameScreenHeight * 0.5f;
+        if (target.Y - GameCore.GameScreenHeight * playerCamera.CameraOffset.Y <= 0)
+            target.Y = GameCore.GameScreenHeight * playerCamera.CameraOffset.Y;
+        else if (target.Y + GameCore.GameScreenHeight * playerCamera.CameraOffset.Y >= MapLogic.GetMapSize().Y)
+            target.Y = MapLogic.GetMapSize().Y - GameCore.GameScreenHeight * playerCamera.CameraOffset.Y;
 
         // Make camera smooth
         // FIXME: fix camera jerkness when almost hitting the target
-        camera.target.X = RayMath.Lerp(camera.target.X, target.X, 1 - (float)Math.Exp(-3 * Raylib.GetFrameTime()));
-        camera.target.Y = RayMath.Lerp(camera.target.Y, target.Y, 1 - (float)Math.Exp(-3 * Raylib.GetFrameTime()));
+        camera.target.X = RayMath.Lerp(camera.target.X, target.X, 1 - (float)Math.Exp(-6 * Raylib.GetFrameTime()));
+        camera.target.Y = RayMath.Lerp(camera.target.Y, target.Y, 1 - (float)Math.Exp(-6 * Raylib.GetFrameTime()));
 
     }
 
--- VortexVise/Models/PlayerCamera.cs
+++ VortexVise/Models/PlayerCamera.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using VortexVise.GameGlobals;
 using ZeroElectric.Vinculum;
 
 namespace VortexVise.Models;
@@ -14,4 +15,15 @@ public class PlayerCamera
     public RenderTexture RenderTexture;
     public Rectangle RenderRectangle;
     public Vector2 CameraPosition;
+    public Vector2 CameraOffset;
+
+    public void SetupCamera(float offsetX, float offsetY)
+    {
+        CameraOffset.X = offsetX;
+        CameraOffset.Y = offsetY;
+        var cameraView = new Vector2(GameCore.GameScreenWidth * offsetX, GameCore.GameScreenHeight * offsetY);
+        Camera.offset = cameraView;
+        Camera.target = cameraView;
+        Camera.zoom = 1;
+    }
 }
