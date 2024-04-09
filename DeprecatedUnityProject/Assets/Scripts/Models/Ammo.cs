b7b02a9a78166a84d9c61f82f99201c55e75115d
Samuel Fontes <samuguel@hotmail.com.br>
2024-04-09 10:14:00 -0500

    Removed unity project

--- DeprecatedUnityProject/Assets/Scripts/Models/Ammo.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Ammo
-{
-    public string Name { get; private set; }
-    public string Description { get; private set; }
-    public int MaxAmount { get; private set; }
-    public int Amount { get; private set; }
-}
