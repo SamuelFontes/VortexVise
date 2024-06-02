951f4f5277856b8955e4b7186426636ce7bcf2df
Samuel Fontes <samuguel@hotmail.com.br>
2024-06-02 09:31:31 -0500

    Added cpp build system

---
+++ VortexVise/VortexVise.h
@@ -0,0 +1,15 @@
+﻿// Dramscape.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+//#include <SDL3/SDL.h>
+//#include <SDL3/SDL_main.h> // only include this one in the source file with main()!
+#include "raylib.h"
+#include "rcamera.h"
+
+#define MAX_COLUMNS 20
+
+
+// TODO: Reference additional headers your program requires here.
