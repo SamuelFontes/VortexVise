b98c7e27800eb38b4a6fe849f5f00f18f474aeee
SamuelFontes <samuguel@hotmail.com.br>
2025-01-03 19:13:43 -0300

    Created GameContext

--- src/VortexVise.Core/GameContext/GameContext.cs
+++ src/VortexVise.Core/GameContext/GameContext.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VortexVise.Core.Interfaces;
 
 namespace VortexVise.Core.GameContext
 {
-    public class GameContext
+    public class GameContext(IAssetService assetService, ICollisionService collisionService, IInputService inputService, IRendererService rendererService, ISoundService soundService)
     {
+        public IAssetService AssetService { get; set; } = assetService;
+        public ICollisionService CollisionService { get; set; } = collisionService;
+        public IInputService InputService { get; set; } = inputService;
+        public IRendererService RendererService { get; set; } = rendererService;
+        public ISoundService SoundService { get; set; } = soundService;
+        public GameCore GameCore { get; set; }
     }
 }
