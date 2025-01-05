32104ed7873409a4ce9c18501b10a964046da4a4
Samuel Fontes <samuguel@hotmail.com.br>
2025-01-05 11:01:16 -0600

    Added webassembly project, needs implementation

---
+++ src/VortexVise.Web/Application.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using Raylib_cs;
+
+namespace RaylibWasm
+{
+    public partial class Application
+    {
+        private static Texture2D logo;
+        
+        /// <summary>
+        /// Application entry point
+        /// </summary>
+        public static void Main()
+        {
+            Raylib.InitWindow(512, 512, "RaylibWasm");
+            Raylib.SetTargetFPS(60);
+            
+            logo = Raylib.LoadTexture("Resources/raylib_logo.png");
+        }
+
+        /// <summary>
+        /// Updates frame
+        /// </summary>
+        [JSExport]
+        public static void UpdateFrame()
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawFPS(4, 4);
+            Raylib.DrawText("All systems operational!", 4, 32, 20, Color.Maroon);
+            
+            Raylib.DrawTexture(logo, 4, 64, Color.White);
+
+            Raylib.EndDrawing();
+        }
+    }
+}
